ce4dda3f3253436ed1313b2b511bd930ac49d6f2
juanpc19 <juan.perez@iesnervion.es>
2023-03-13 10:02:08 +0100

    ej1 hecho y comentado

--- Tema7EjerciciosPOO/src/ejercicio1/CuentaCorriente.java
+++ Tema7EjerciciosPOO/src/ejercicio1/CuentaCorriente.java
@@ -32,22 +32,44 @@ public class CuentaCorriente {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * metodo que sacara dinero si existe saldo suficiente para ello
+	 * 
+	 * @param dineroSolicitado guardara la cantidad de dinero a extraer
+	 */
 	void sacarDinero(double dineroSolicitado) {
-		if (saldo > dineroSolicitado) {
+		// si el atributo saldo actual es mayor o igual que el dinero a extraer
+		if (saldo >= dineroSolicitado) {
+			// se llevara a cabo la extraccion restandole dineroSolicitado a atributo saldo
 			saldo -= dineroSolicitado;
+			// y se le comunicara al usuario mediante mensaje por pantalla
 			System.out.println("Su transaccion es posible y se ha llevado a cabo. ");
 
+			// de lo contrario no se llevara a cabo la operacion
+			// y se le comunicara al usuario mediante mensaje por pantalla
 		} else {
 			System.out.println("Su transaccion no es posible, no tiene suficiente saldo. ");
 		}
 	}
 
+	/**
+	 * metodo que añadira el valor del dinero ingresado a saldo
+	 * 
+	 * @param dineroIngresar
+	 */
 	void ingresarDinero(double dineroIngresar) {
+		// añado el valor de dineroIngresar a atributo saldo
 		saldo += dineroIngresar;
 	}
 
-	//void mostrarInformacion
-	
-	
-	
+	/**
+	 * metodo que mostrara los datos de la cuenta especificada asociada con su
+	 * propio objeto
+	 */
+	void mostrarInformacion() {
+		// print de pantalla de los atributos con los valores del objeto con el que se
+		// usa el metodo
+		System.out.println(dni + " " + nombre + " " + saldo);
+	}
+
 }
