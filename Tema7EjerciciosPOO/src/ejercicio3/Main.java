4e5762f3183d35dc0b021186a20ebdb7c32cd5c8
juanpc19 <juan.perez@iesnervion.es>
2023-03-14 12:50:29 +0100

    d

--- Tema7EjerciciosPOO/src/ejercicio3/Main.java
+++ Tema7EjerciciosPOO/src/ejercicio3/Main.java
@@ -3,7 +3,12 @@ package ejercicio3;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Punto p= new Punto(20,10);
+		
+		Punto p2 = new Punto(-20,10);
+		
+		
 
 	}
 
--- Tema7EjerciciosPOO/src/ejercicio3/Punto.java
+++ Tema7EjerciciosPOO/src/ejercicio3/Punto.java
@@ -2,4 +2,34 @@ package ejercicio3;
 
 public class Punto {
 
+	int x;
+
+	int y;
+
+	public Punto(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	void imprime() {
+		System.out.println("(" + x + "," + y + ")");
+	}
+
+	void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	void desplaza(int dx, int dy) {
+		x += dx;
+		y += dy;
+	}
+
+	void distancia(Punto p) {
+		double distancia = 0;
+
+		Math.sqrt(Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2));
+		System.out.println(distancia);
+	}
 }
